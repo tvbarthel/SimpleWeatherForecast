7270b776294607c1c0395181bfdcd501a27e20f1
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-12-23 17:30:24 +0100

    Tweak the Ui.

--- SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/MainActivity.java
+++ SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/MainActivity.java
@@ -1,5 +1,8 @@
 package fr.tvbarthel.apps.simpleweatherforcast;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,6 +17,7 @@ import android.view.View;
 import java.util.Locale;
 
 import fr.tvbarthel.apps.simpleweatherforcast.fragments.ForecastFragment;
+import fr.tvbarthel.apps.simpleweatherforcast.utils.ColorUtils;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -24,15 +28,49 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.argb(130, 0, 0, 0)));
+
 		mSectionsPagerAdapter = new ForecastPagerAdapter(getSupportFragmentManager());
 		mViewPager = (ViewPager) findViewById(R.id.pager);
 		mViewPager.setAdapter(mSectionsPagerAdapter);
 		mViewPager.setPageTransformer(true, new ForecastPageTransformer());
 		mViewPager.setOffscreenPageLimit(2);
+		mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			@Override
+			public void onPageScrolled(int i, float v, int i2) {
+				final GradientDrawable g = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,
+						new int[]{getColor(i, v), getColor(i, (float) Math.sqrt(v))});
+				getWindow().setBackgroundDrawable(g);
+			}
 
-		getWindow().setBackgroundDrawableResource(R.color.background_default);
+			@Override
+			public void onPageSelected(int i) {
+
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int i) {
+			}
+		});
+
+		mViewPager.setBackground(new ColorDrawable(Color.argb(60, 0, 0, 0)));
+
 	}
 
+	private int getColor(int currentPosition, float positionOffset) {
+		//retrieve current color and next color relative to the current position.
+		final int colorSize = ColorUtils.COLORS.length - 1;
+		final int[] currentColor = ColorUtils.COLORS[(currentPosition) % colorSize];
+		final int[] nextColor = ColorUtils.COLORS[(currentPosition + 1) % colorSize];
+
+		//Compute the deltas relative to the current position offset.
+		final int deltaR = (int) ((nextColor[0] - currentColor[0]) * positionOffset);
+		final int deltaG = (int) ((nextColor[1] - currentColor[1]) * positionOffset);
+		final int deltaB = (int) ((nextColor[2] - currentColor[2]) * positionOffset);
+
+		return Color.argb(255, currentColor[0] + deltaR, currentColor[1] + deltaG, currentColor[2] + deltaB);
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -84,12 +122,12 @@ public class MainActivity extends ActionBarActivity {
 					view.setAlpha(0);
 
 				} else if (position <= 2.0) { // [-2.0,2.0]
-					final float normalizedPosition =  Math.abs(position / 2);
+					final float normalizedPosition = Math.abs(position / 2);
 					final float scaleFactor = 1f - normalizedPosition;
 					final float horizontalMargin = pageWidth * normalizedPosition;
 
 					//Translate back the page.
-					if(position < 0) {
+					if (position < 0) {
 						//left
 						view.setTranslationX(horizontalMargin);
 					} else {
@@ -98,7 +136,7 @@ public class MainActivity extends ActionBarActivity {
 					}
 
 					// Scale the page down relative to its size.
-					view.setScaleX((float)Math.pow(scaleFactor, 0.80));
+					view.setScaleX((float) Math.pow(scaleFactor, 0.80));
 					view.setScaleY((float) Math.pow(scaleFactor, 0.80));
 
 					// Fade the page relative to its size.
