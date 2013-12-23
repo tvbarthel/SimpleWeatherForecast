ad5a0249c0bb8735a24386193e37e09ea5816f42
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-12-23 14:44:16 +0100

    Add ForecastFragment skeleton.

--- SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/MainActivity.java
+++ SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/MainActivity.java
@@ -1,89 +1,116 @@
 package fr.tvbarthel.apps.simpleweatherforcast;
 
-import java.util.Locale;
-
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+
+import java.util.Locale;
 
 import fr.tvbarthel.apps.simpleweatherforcast.fragments.ForecastFragment;
 
 public class MainActivity extends ActionBarActivity {
 
-    ForecastPagerAdapter mSectionsPagerAdapter;
-    ViewPager mViewPager;
+	ForecastPagerAdapter mSectionsPagerAdapter;
+	ViewPager mViewPager;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mSectionsPagerAdapter = new ForecastPagerAdapter(getSupportFragmentManager());
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mSectionsPagerAdapter = new ForecastPagerAdapter(getSupportFragmentManager());
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mSectionsPagerAdapter);
+		mViewPager.setPageTransformer(true, new ForecastPageTransformer());
+		mViewPager.setOffscreenPageLimit(2);
 
 		getWindow().setBackgroundDrawableResource(R.color.background_default);
-    }
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    
 
-    public class ForecastPagerAdapter extends FragmentPagerAdapter {
+	public class ForecastPagerAdapter extends FragmentPagerAdapter {
 
-        public ForecastPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
+		public ForecastPagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
 
-        @Override
-        public Fragment getItem(int position) {
-            return new ForecastFragment();
-        }
+		@Override
+		public Fragment getItem(int position) {
+			return ForecastFragment.newInstance(position);
+		}
 
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
+		@Override
+		public int getCount() {
+			// Show 14 total pages.
+			return 14;
+		}
 
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
-            }
-            return null;
-        }
-    }
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return ("Section " + String.valueOf(position)).toUpperCase(Locale.getDefault());
+		}
+	}
+
+	public class ForecastPageTransformer implements ViewPager.PageTransformer {
+
+		public void transformPage(View view, float position) {
+			final int pageWidth = view.getWidth();
+
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+				if (position < -2.0) { // [-Infinity,-2.0)
+					// This page is way off-screen to the left.
+					view.setAlpha(0);
+
+				} else if (position <= 2.0) { // [-2.0,2.0]
+					final float normalizedPosition =  Math.abs(position / 2);
+					final float scaleFactor = 1f - normalizedPosition;
+					final float horizontalMargin = pageWidth * normalizedPosition;
+
+					//Translate back the page.
+					if(position < 0) {
+						//left
+						view.setTranslationX(horizontalMargin);
+					} else {
+						//right
+						view.setTranslationX(-horizontalMargin);
+					}
+
+					// Scale the page down relative to its size.
+					view.setScaleX((float)Math.pow(scaleFactor, 0.80));
+					view.setScaleY((float) Math.pow(scaleFactor, 0.80));
+
+					// Fade the page relative to its size.
+					view.setAlpha(1f * scaleFactor - (scaleFactor * 0.25f));
+
+				} else { // (2.0,+Infinity]
+					// This page is way off-screen to the right.
+					view.setAlpha(0);
+				}
+			}
+
+		}
+	}
 
 }
--- SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/fragments/ForecastFragment.java
+++ SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/fragments/ForecastFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import fr.tvbarthel.apps.simpleweatherforcast.R;
 
@@ -13,9 +14,20 @@ import fr.tvbarthel.apps.simpleweatherforcast.R;
  */
 public class ForecastFragment extends Fragment {
 
+	public static String ARGUMENT_POSITION = "Position";
+
+	public static ForecastFragment newInstance(int position) {
+		final ForecastFragment instance = new ForecastFragment();
+		final Bundle arguments = new Bundle();
+		arguments.putInt(ARGUMENT_POSITION, position);
+		instance.setArguments(arguments);
+		return instance;
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View v = inflater.inflate(R.layout.fragment_forecast, container, false);
+		((TextView)v.findViewById(R.id.forecast_fragment_position)).setText(String.valueOf(getArguments().getInt(ARGUMENT_POSITION)));
 		return v;
 	}
 }
