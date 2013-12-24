77fea33bcb49dffed0209f771da3812e28957bfb
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-12-24 18:19:08 +0100

    Change to HttpUrlConnection.

--- SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/utils/URLUtils.java
+++ SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/utils/URLUtils.java
@@ -1,23 +1,27 @@
 package fr.tvbarthel.apps.simpleweatherforcast.utils;
 
 
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class URLUtils {
 
 	public static String getAsString(String urlString) throws MalformedURLException, IOException {
 		final URL url = new URL(urlString);
-		final URLConnection connection = url.openConnection();
-		connection.setUseCaches(false);
-		connection.setConnectTimeout(5000);
-		connection.setReadTimeout(10000);
 
-		final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		final HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+		urlConnection.setUseCaches(false);
+
+		//TODO handle the response code.
+		Log.d("argonne", "code ->" + urlConnection.getResponseCode());
+
+		final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
 		final StringBuilder stringBuilder = new StringBuilder();
 		String line;
 		while ((line = bufferedReader.readLine()) != null) {
@@ -26,4 +30,5 @@ public class URLUtils {
 		}
 		return stringBuilder.toString();
 	}
+
 }
