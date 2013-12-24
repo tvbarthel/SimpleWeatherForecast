b52d9c60b89922dd1ab133623b4e2ee56065b1aa
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-12-24 15:40:57 +0100

    Tweak colors a bit.

--- SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/utils/ColorUtils.java
+++ SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/utils/ColorUtils.java
@@ -5,13 +5,16 @@ public class ColorUtils {
 
 	public static int[][] COLORS = new int[][]{
 			new int[]{51, 181, 229}, //#33B5E5 holo blue
-			new int[]{0, 153, 204}, //#0099CC holo blue dark
-			new int[]{153, 51, 204}, //#9933CC holo purple dark
 			new int[]{170, 102, 204}, //#AA66CC holo purple
-			new int[]{255, 68, 68}, //#FF4444 holo red
-			new int[]{255, 136, 0}, //#FF8800 holo yellow dark
 			new int[]{255, 187, 51}, //#FFBB33 holo yellow
+			new int[]{255, 68, 68}, //#FF4444 holo red
 			new int[]{153, 204, 0}, //#99CC00 holo green
-			new int[]{102, 153, 10}, //#669900 holo green dark
+
+
+			//Other holo colors.
+			//new int[]{255, 136, 0}, //#FF8800 holo yellow dark
+			//new int[]{0, 153, 204}, //#0099CC holo blue dark
+			//new int[]{153, 51, 204}, //#9933CC holo purple dark
+			//new int[]{102, 153, 10}, //#669900 holo green dark
 	};
 }
