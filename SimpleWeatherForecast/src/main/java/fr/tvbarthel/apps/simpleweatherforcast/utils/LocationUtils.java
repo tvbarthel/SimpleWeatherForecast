4b16df257fd547a0b58a9548d16a8c091c3e046e
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-12-26 11:18:16 +0100

    Add a LocationUtils to retrieve the last known location.

---
+++ SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/utils/LocationUtils.java
@@ -0,0 +1,24 @@
+package fr.tvbarthel.apps.simpleweatherforcast.utils;
+
+
+import android.app.Service;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+
+public class LocationUtils {
+
+	public static Location getLastKnownLocation(Context context) {
+		Location lastKnownLocation = null;
+		final LocationManager locationManager = (LocationManager) context.getSystemService(Service.LOCATION_SERVICE);
+		final Criteria locationCriteria = new Criteria();
+		locationCriteria.setAccuracy(Criteria.ACCURACY_COARSE);
+		final String providerName = locationManager.getBestProvider(locationCriteria, true);
+		if (providerName != null) {
+			lastKnownLocation = locationManager.getLastKnownLocation(providerName);
+		}
+
+		return lastKnownLocation;
+	}
+}
