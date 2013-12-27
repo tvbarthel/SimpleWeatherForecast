16a1588ed3698be84f152cadf2d1c7c3d80e63fa
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2013-12-27 12:11:20 +0100

    Handle response code.

--- SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/utils/URLUtils.java
+++ SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/utils/URLUtils.java
@@ -1,34 +1,35 @@
 package fr.tvbarthel.apps.simpleweatherforcast.utils;
 
 
-import android.util.Log;
+import org.apache.http.HttpStatus;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class URLUtils {
 
-	public static String getAsString(String urlString) throws MalformedURLException, IOException {
+	public static String getAsString(String urlString) throws IOException {
 		final URL url = new URL(urlString);
+		String resultContent = null;
 
 		final HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 		urlConnection.setUseCaches(false);
 
-		//TODO handle the response code.
-		Log.d("argonne", "code ->" + urlConnection.getResponseCode());
-
-		final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-		final StringBuilder stringBuilder = new StringBuilder();
-		String line;
-		while ((line = bufferedReader.readLine()) != null) {
-			line += "\n";
-			stringBuilder.append(line);
+		if (urlConnection.getResponseCode() == HttpStatus.SC_OK) {
+			final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+			final StringBuilder stringBuilder = new StringBuilder();
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				line += "\n";
+				stringBuilder.append(line);
+			}
+			resultContent = stringBuilder.toString();
 		}
-		return stringBuilder.toString();
+
+		return resultContent;
 	}
 
 }
