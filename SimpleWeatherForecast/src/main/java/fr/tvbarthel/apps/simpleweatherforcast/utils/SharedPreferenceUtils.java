1be14428d122e409917fe8815645e346fc27cfb0
Vincent Barthélémy <vincent.barthelemy.perso@gmail.com>
2014-02-07 21:08:56 +0100

    Implement method to store and get the temperature unit

--- SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/utils/SharedPreferenceUtils.java
+++ SimpleWeatherForecast/src/main/java/fr/tvbarthel/apps/simpleweatherforcast/utils/SharedPreferenceUtils.java
@@ -6,9 +6,12 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.preference.PreferenceManager;
 
+import fr.tvbarthel.apps.simpleweatherforcast.R;
+
 public class SharedPreferenceUtils {
 	private static String KEY_LAST_UPDATE = "SharedPreferenceUtils.Key.LastUpdate";
 	private static String KEY_LAST_KNOWN_JSON_WEATHER = "SharedPreferenceUtils.Key.LastKnownJsonWeather";
+	private static String KEY_TEMPERATURE_UNIT_SYMBOL = "SharedPreferenceUtils.Key.TemperatureUnitSymbol";
 
 	private static SharedPreferences getDefaultSharedPreferences(final Context context) {
 		return PreferenceManager.getDefaultSharedPreferences(context);
@@ -27,6 +30,17 @@ public class SharedPreferenceUtils {
 		editor.putString(KEY_LAST_KNOWN_JSON_WEATHER, jsonWeather);
 		editor.putLong(KEY_LAST_UPDATE, System.currentTimeMillis());
 		editor.apply();
+	}
+
+	public static void storeTemperatureUnitSymbol(final Context context, final String unitSymbol) {
+		final Editor editor = getDefaultSharedPreferences(context).edit();
+		editor.putString(KEY_TEMPERATURE_UNIT_SYMBOL, unitSymbol);
+		editor.apply();
+	}
+
+	public static String getTemperatureUnitSymbol(final Context context) {
+		return getDefaultSharedPreferences(context).getString(KEY_TEMPERATURE_UNIT_SYMBOL,
+				context.getString(R.string.temperature_unit_celsius_symbol));
 	}
 
 }
